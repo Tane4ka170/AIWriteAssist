28725238a36fd6312876077804205b3ba3ca2d39
Tane4ka170 <tane4ka170@gmail.com>
2025-02-10 17:02:10 +0200

    Improved server setup for API endpoints

--- server/app.js
+++ server/app.js
@@ -1,5 +1,18 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config;
+require("dotenv").config();
+const analyzeRoute = require("./routes/analyze");
+const grammarCheckRoute = require("./routes/grammarCheck");
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/analyze", analyzeRoute);
+app.use("/api/grammarCheck", grammarCheckRoute);
+
+app.listen(port, () => {
+  console.log(`Server on port: ${port}`);
+});
