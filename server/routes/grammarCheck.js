93fb080d75582d4eb5a72e4e78f30c18e447c84b
Tane4ka170 <tane4ka170@gmail.com>
2025-02-11 17:44:22 +0200

    Added OpenAI API call to grammarCheckRoute

--- server/routes/grammarCheck.js
+++ server/routes/grammarCheck.js
@@ -1,10 +1,47 @@
 const express = require("express");
+const axios = require("axios");
 const grammarCheckRoute = express.Router();
 
 grammarCheckRoute.post("/", async (req, res) => {
-  res.json({ message: "Analyze Route" });
+  const { text } = req.body;
+  try {
+    const response = await axios.post(
+      "https://api.openai.com/v1/chat/completions",
+      {
+        model: "gpt-4o-mini",
+        messages: [
+          {
+            role: "system",
+            content:
+              "You are a reliable assistant that reviews and fixes grammar mistakes in the given text. Provide only the corrected version without any extra comments or context",
+          },
+          {
+            role: "user",
+            content: `${sentence}`,
+          },
+        ],
+        max_tokens: 150,
+        n: 1,
+        stop: null,
+        temperature: 0.7,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.OPENAI_SECRET_KEY}`,
+        },
+      }
+    );
+
+    const correctedText = response.data.choices[0].message.content;
+
+    res.json({ correctedText });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
 });
 
 module.exports = {
-  analyzeRoute,
+  grammarCheckRoute,
 };
