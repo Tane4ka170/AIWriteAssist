2ede51b3fcba5e95bcc5d344629558be56b82762
Tane4ka170 <tane4ka170@gmail.com>
2025-02-11 17:43:01 +0200

    Added OpenAI API call to analyzeRoute

--- server/routes/analyze.js
+++ server/routes/analyze.js
@@ -1,8 +1,47 @@
 const express = require("express");
+const axios = require("axios");
 const analyzeRoute = express.Router();
 
 analyzeRoute.post("/", async (req, res) => {
-  res.json({ message: "Analyze Route" });
+  const { sentence } = req.body;
+  try {
+    const response = await axios.post(
+      "https://api.openai.com/v1/chat/completions",
+      {
+        model: "gpt-4o-mini",
+        messages: [
+          {
+            role: "system",
+            content:
+              "You are an efficient assistant that rewrites sentences. Provide only the revised sentences without extra explanations or context",
+          },
+          {
+            role: "user",
+            content: `${sentence}`,
+          },
+        ],
+        max_tokens: 150,
+        n: 10,
+        stop: null,
+        temperature: 0.7,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.OPENAI_SECRET_KEY}`,
+        },
+      }
+    );
+
+    const rephrasedSentences = response.data.choices.map(
+      (choice) => choice.message.content
+    );
+
+    res.status(200).json(rephrasedSentences || []);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
 });
 
 module.exports = {
