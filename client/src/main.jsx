9e7b7372e62c016d498c42c0a8ea73039cabe41a
Tane4ka170 <tane4ka170@gmail.com>
2025-02-14 20:11:31 +0200

    Updated PrivyProvider configuration

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,20 +1,22 @@
 import { createRoot } from "react-dom/client";
+import { PrivyProvider } from "@privy-io/react-auth";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import ai from "./images/ai.png";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <PrivyProvider
-      appId="your-privy-app-id"
+      appId="cm73dnc7a01c3ifvjvvwulr4w"
       config={{
         // Display email and wallet as login methods
-        loginMethods: ["email", "wallet"],
+        loginMethods: ["email", "wallet" / "google", "github"],
         // Customize Privy's appearance in your app
         appearance: {
           theme: "light",
-          accentColor: "#676FFF",
-          logo: "https://your-logo-url",
+          accentColor: "#b79a46",
+          logo: ai,
         },
         // Create embedded wallets for users who don't have a wallet
         embeddedWallets: {
